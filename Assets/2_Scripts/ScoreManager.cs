d9fc28f53e29a323eaeb34cf90bb3e115b7aaba8
ghdrudtj <ghdqwer432@gmail.com>
2024-08-27 12:24:01 +0900

    ㅇ

--- Assets/2_Scripts/ScoreManager.cs
+++ Assets/2_Scripts/ScoreManager.cs
@@ -9,33 +9,67 @@ using UnityEngine.XR;
 
 public class ScoreManager : MonoBehaviour
 {
+    private struct ScoreData
+    {
+        public string str;
+        public Color color;
+        public Vector2 pos;
+    }
     public static ScoreManager instance;
     [SerializeField] private TextMeshProUGUI scoreTmp;
     [SerializeField] private TextMeshProUGUI bonusTmp;
     [SerializeField] private Score baseScore;
+    private List<ScoreData> scoreDataList = new List<ScoreData>();
 
     private int totalScore;
     private float totalBonus;
     public void lnit()
     {
         instance = this;
+        StartCoroutine(OnScoreCor());
+    }
+    private IEnumerator OnScoreCor()
+    {
+        while(true)
+        {
+            if(scoreDataList.Count > 0)
+            {
+                ScoreData data = scoreDataList[0];
+
+                Score scoreObj = Instantiate<Score>(baseScore);
+                scoreObj.transform.position = data.pos;
+                scoreObj.Active(data.str, data.color);
+
+                scoreDataList.RemoveAt(0);
+                yield return new WaitForSeconds(DataBaseManager.Instance.ScorePopInterval);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
     }
     public void AddScore(int score, Vector2 scorePos)
     {
-        Score scoreObject = Instantiate(baseScore);
-        scoreObject.transform.position = scorePos;
-        scoreObject.Active(score.ToString(), DataBaseManager.Instance.ScoreColor);
-
+        //¾Ö´Ï
+        scoreDataList.Add(new ScoreData()
+        {
+            str = score.ToString(),
+            color = DataBaseManager.Instance.ScoreColor,
+            pos = scorePos
+        });
+        //Canvas
         totalScore += score;
         scoreTmp.text = totalScore.ToString();
     }
     internal void AddBonus(float bonus, Vector2 position)
     {
-        Score scoreObject = Instantiate(baseScore);
-        scoreObject.transform.position = position;
-
-        string str = "Bouns" + bonus.ToPercentString();
-        scoreObject.Active(str, DataBaseManager.Instance.BonusColor);
+        scoreDataList.Add(new ScoreData()
+        {
+            str = "Bouns" + bonus.ToPercentString(),
+            color = DataBaseManager.Instance.BonusColor,
+            pos = position
+        });
 
         totalBonus += bonus;
         bonusTmp.text = totalBonus.ToPercentString();
